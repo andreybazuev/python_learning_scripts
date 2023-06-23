f3730e4b8de2f9566d67153ef11793983fcb80f0
Hermes <andreybazuev@gmail.com>
2023-06-23 17:23:19 +0300

    23.06

---
+++ find_letter.py
@@ -0,0 +1,9 @@
+def find(word, letter, ix): # ix - с какого индекса начинать поиск
+    index = ix
+    while index < len(word):
+        if word[index] == letter:
+            return index
+        index = index + 1
+    return "the letter is not in the word"
+
+print(find('abcdefg', 'd', 3))
