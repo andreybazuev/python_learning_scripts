b081ab4596a9eb099d48bbf1ff5008190cbc0bda
Hermes <andreybazuev@gmail.com>
2023-06-26 18:27:16 +0300

    -

--- find_all_letter.py
+++ find_all_letter.py
@@ -1,7 +1,7 @@
 #поиск всех индексов искомой буквы в строке
 
 
-def find(word, letter): # ix - с какого индекса начинать поиск!
+def find(word, letter):
     index = 0
     print ('Длина слова в символах:', len(word))
     while index < len(word):
